fc257f94b44622aafa7e7cb52e12c8bb5106f540
JinJieBeWater <cjj20030813@qq.com>
2025-02-03 10:24:50 +0800

    feat(diff): implement diffEdit functionality for enhanced text editing and decoration

---
+++ src/editor/diffEdit.ts
@@ -0,0 +1,77 @@
+import type { TextEditor, TextEditorDecorationType } from 'vscode'
+import { Range } from 'vscode'
+import type { Edit } from '../diff/computeDiff'
+import { EditType } from '../diff/computeDiff'
+import { createDeletionDecoration, createInsertedDecoration } from './decoration'
+
+interface lifeCycleInstance {
+  decorations: TextEditorDecorationType[]
+  textEditor: TextEditor
+  edit: Edit
+}
+
+function useInsertionDecoration(instance: lifeCycleInstance) {
+  const { textEditor, decorations, edit } = instance
+  setImmediate(() => {
+    decorations.push(createInsertedDecoration())
+    decorations.forEach((decoration) => {
+      textEditor.setDecorations(decoration, [edit.range])
+    })
+  })
+}
+
+function useDeletionDecoration(instance: lifeCycleInstance) {
+  const { textEditor, decorations, edit } = instance
+  if (edit.type === EditType.DecoratedReplacement) {
+    const lines = edit.oldText.split('\n')
+    const position = edit.range.start
+    lines.forEach((line) => {
+      const decoration = createDeletionDecoration(line)
+      decorations.push(decoration)
+      textEditor.setDecorations(decoration, [new Range(position, position)])
+    })
+  }
+  else {
+    throw new Error('createDeletionDecoration only support EditType.DecoratedReplacement')
+  }
+}
+
+export async function diffEditItem(textEditor: TextEditor, part: Edit): Promise<lifeCycleInstance> {
+  const instance: lifeCycleInstance = {
+    decorations: [],
+    textEditor,
+    edit: part,
+  }
+  await textEditor.edit((editBuilder) => {
+    switch (part.type) {
+      case EditType.Insertion:
+        editBuilder.insert(part.range.start, part.text)
+        useInsertionDecoration(instance)
+        break
+      case EditType.Deletion:
+        editBuilder.delete(part.range)
+        break
+      case EditType.DecoratedReplacement:
+        editBuilder.replace(part.range, part.text)
+        useDeletionDecoration(instance)
+        break
+      default:
+        throw new Error('Unknown edit type')
+    }
+  }).then(async (success) => {
+    if (!success) {
+      await diffEditItem(textEditor, part)
+    }
+  })
+
+  return instance
+}
+
+export async function diffEdit(textEditor: TextEditor, diff: Edit[]) {
+  const instances: lifeCycleInstance[] = []
+  for (const part of diff) {
+    const instance = await diffEditItem(textEditor, part)
+    instances.push(instance)
+  }
+  return instances
+}
--- src/magic/index.ts
+++ src/magic/index.ts
@@ -1,71 +1,22 @@
-import type { TextEditor, TextEditorDecorationType } from 'vscode'
-import { Position, Range, window } from 'vscode'
+import { window } from 'vscode'
 import type { Magic } from '../types/magic'
-import type { MessageButler } from '../AISDK'
 import { createMessageButler } from '../AISDK'
 import { getSelectedText } from '../utils/getSelectedText'
 import { createGenerateText } from '../AISDK/createGenerateText'
-import type { Edit } from '../diff/computeDiff'
-import { EditType, computeDiff } from '../diff/computeDiff'
-import { createDeletionDecoration, createInsertedDecoration } from '../editor/decoration'
-
-interface Instance {
-  decoration: TextEditorDecorationType[]
-}
+import { computeDiff } from '../diff/computeDiff'
+import { diffEdit } from '../editor/diffEdit'
 
 export async function sparkMagic(magic: Magic) {
   const textEditor = window.activeTextEditor!
   const originalText = getSelectedText(textEditor!)
   const msgButler = createMessageButler().addUser(originalText, magic.prompt)
 
-  const fullResponse = await createGenerateText(msgButler.messages)
+  const replacement = await createGenerateText(msgButler.messages)
 
-  const diff = computeDiff(fullResponse, originalText, textEditor.selection, {
+  const diff = computeDiff(replacement, originalText, textEditor.selection, {
     decorateDeletions: true,
   })
 
-  const diffEdit = async (part: Edit) => {
-    const instance: Instance = {
-      decoration: [],
-    }
-    await textEditor.edit((editBuilder) => {
-      switch (part.type) {
-        case EditType.Insertion:
-          editBuilder.insert(part.range.start, part.text)
-          setImmediate(() => {
-            instance.decoration.push(createInsertedDecoration())
-            instance.decoration.forEach((decoration) => {
-              textEditor.setDecorations(decoration, [part.range])
-            })
-          })
-          break
-        case EditType.Deletion:
-          editBuilder.delete(part.range)
-          break
-        case EditType.DecoratedReplacement:
-          editBuilder.replace(part.range, part.text)
-          setImmediate(() => {
-            const lines = part.oldText.split('\n')
-            let position = part.range.start
-            lines.forEach((line) => {
-              const decoration = createDeletionDecoration(line)
-              instance.decoration.push(decoration)
-              textEditor.setDecorations(decoration, [new Range(position, position)])
-              position = new Position(position.line + 1, 0)
-            })
-          })
-          break
-        default:
-          throw new Error('Unknown edit type')
-      }
-    }).then(async (success) => {
-      if (!success) {
-        await diffEdit(part)
-      }
-    })
-  }
-
-  for (const part of diff) {
-    await diffEdit(part)
-  }
+  const instances = await diffEdit(textEditor, diff)
+  return instances
 }
