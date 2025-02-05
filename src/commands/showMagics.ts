24f17ad3d8733ef6dd8d833effc649a0baf14317
JinJieBeWater <cjj20030813@qq.com>
2025-02-05 13:41:50 +0800

    feat: use watchEffect to manage decoration

--- src/commands/showMagics.ts
+++ src/commands/showMagics.ts
@@ -1,9 +1,12 @@
 import useMagicQuickPick from '../editor/useMagicQuickPick'
 import { selectAiLine } from '../editor/selectAiLine'
+import { logger } from '../utils/logger'
 
 export async function showMagics(args: number) {
-  if (args !== undefined)
+  if (args !== undefined) {
+    logger.info('showMagics', args)
     selectAiLine(args)
+  }
 
   const qp = useMagicQuickPick()
   qp.show()
