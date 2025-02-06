4ea7b847272b1ab86f1f5ab2eed908e3fc6be155
JinJieBeWater <cjj20030813@qq.com>
2025-02-06 18:52:38 +0800

    feat: add cancel command for CodeLens and refactor loading logic in connectAISDK

---
+++ src/AISDK/connectAISDK.ts
@@ -0,0 +1,35 @@
+import type { CoreMessage } from 'ai'
+import { generateText } from 'ai'
+import { type Selection, window } from 'vscode'
+import { StatusCodelensProvider } from '../editor/codelens/StatusCodelensProvider'
+import { logger } from '../utils/logger'
+import { getModel } from './getModel'
+
+export async function connectAISDK({ messages, selection }: { messages: CoreMessage[], selection: Selection }) {
+  const loadingCodelens = new StatusCodelensProvider(selection)
+
+  try {
+    const result = await generateText({
+      model: getModel(),
+      messages,
+      abortSignal: loadingCodelens.abortController.signal,
+    })
+    // 预防服务器没有响应
+    if (result.text === '') {
+      window.showErrorMessage('No response from the server')
+    }
+    else {
+      return result.text
+    }
+  }
+  catch (error) {
+    if (!loadingCodelens.abortController.signal.aborted) {
+      window.showErrorMessage('createGenerateText', JSON.stringify(error))
+      logger.error('createGenerateText', JSON.stringify(error))
+      throw error
+    }
+  }
+  finally {
+    loadingCodelens.dispose()
+  }
+}
