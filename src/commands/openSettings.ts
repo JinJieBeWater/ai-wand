2e87b4002706571fceb3787823bf9512b3f6d9f9
JinJieBeWater <cjj20030813@qq.com>
2025-02-08 03:42:05 +0800

    feat: add toggle provider command to switch between AI providers in Magic Wand

--- src/commands/openSettings.ts
+++ src/commands/openSettings.ts
@@ -2,5 +2,5 @@ import { commands } from 'vscode'
 import * as Meta from '../generated/meta'
 
 export function openMagicsSettings() {
-  commands.executeCommand('workbench.action.openSettings', `${Meta.name}.magics`)
+  commands.executeCommand('workbench.action.openSettings', `${Meta.name}.`)
 }
